f9670ac4483867dd67830a08b2fd42388124dfc3
NeemiasB <36780438+NeemiasB@users.noreply.github.com>
2018-07-01 21:14:15 -0400

    Add files via upload

---
+++ WebAppCurriculo/Controllers/CurriculoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCurriculo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppCurriculo.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View(new Curriculo());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Curriculo curriculo)
+        {
+            return View(curriculo);
+        }
+    }
+}
---
+++ WebAppCurriculo/Models/Curriculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurriculo.Models
+{
+    public class Curriculo
+    {
+        //INFORMAÇÕES PESSOAIS
+        public int CurriculoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public decimal Altura { get; set; }
+        public string Site { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public Sexo Sexo { get; set; }
+
+        //ENDEREÇO
+        public string Endereco { get; set; }
+        public string CEP { get; set; }
+
+        //CONTATO
+        public string TelefoneFixo { get; set; }
+        public string TelefoneMovel { get; set; }
+
+        //CONHECIMENTOS
+        public bool HTML { get; set; }
+        public bool CSS { get; set; }
+        public bool Javascript { get; set; }
+        public string MaisConhecimentos { get; set; }
+
+
+    }
+}
